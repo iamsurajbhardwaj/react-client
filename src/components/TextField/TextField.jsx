cc1ac41449ca60f16980b81290d4350df59ad21a
iamsurajbhardwaj <suraj.bhardwaj@successive.tech>
2019-02-18 11:27:52 +0530

    feat: 🎸 create generic inpput component & use in web page
    create a generic component and use that multiple times in web page with
    different variations

---
+++ src/components/TextField/TextField.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import textFieldStyle from './style';
+
+const TextField = () => (
+  <input type="text" style={textFieldStyle} />
+);
+export default TextField;
---
+++ src/components/TextField/style.js
@@ -0,0 +1,11 @@
+const textFieldStyle = {
+  margin: '40px',
+  border: '5px solid pink',
+  borderRadius: '20px',
+  display: 'flex',
+};
+// const pStyle = {
+//   fontSize: '15px',
+//   textAlign: 'center',
+// };
+export default textFieldStyle;
