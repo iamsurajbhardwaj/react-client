968cd291b063ad2617a789a0ae3646dcd17d3ab9
iamsurajbhardwaj <suraj.bhardwaj@successive.tech>
2019-02-19 13:56:25 +0530

    feat: 🎸 add slider banner at web page

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { getRandomNumber, getRoundRobinNumber } from '../../lib/utils/math';
+import { DEFAULT_BANNER_IMAGE } from '../../configs/constants';
+
+const defaultProps = {
+  altText: 'Default Banner',
+  banner: '',
+  defaultBanner: DEFAULT_BANNER_IMAGE,
+  duration: 2000,
+  height: 200,
+  random: false,
+};
+const propTypes = {
+  altText: PropTypes.string,
+  banner: PropTypes.arr,
+  defaultBanner: PropTypes.string,
+  duration: PropTypes.number,
+  height: PropTypes.number,
+  random: PropTypes.boolean,
+};
+
+
+class Slider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { index: 0 };
+  }
+
+  componentDidMount() {
+    const { random, duration, banner } = this.props;
+    const totalImage = banner.length;
+    this.timerId = setInterval(() => {
+      const { index } = this.state;
+      this.setState({
+        index: (random) ? (getRandomNumber(totalImage)) : (getRoundRobinNumber(index, totalImage)),
+      });
+    }, duration);
+  }
+
+  componentWillUnmount = () => {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    const {
+      altText,
+      banner,
+      random,
+      defaultBanner,
+      height,
+      ...rest
+    } = this.props;
+    const { index } = this.state;
+    const imagePath = banner ? banner[index] : defaultBanner;
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <img src={imagePath} alt="xyz" {...rest} style={{ height: '200px' }} />
+      </div>
+    );
+  }
+}
+Slider.propTypes = propTypes;
+Slider.defaultProps = defaultProps;
+export default Slider;
